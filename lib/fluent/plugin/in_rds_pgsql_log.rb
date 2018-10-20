46652c00fd779a61bb0ab686b8b4e3f0226cac20
Christoph Grabo <christoph@contentful.com>
2018-10-20 20:07:06 +0200

    Use only the rds and ec2 resources from aws-sdk

--- lib/fluent/plugin/in_rds_pgsql_log.rb
+++ lib/fluent/plugin/in_rds_pgsql_log.rb
@@ -1,5 +1,6 @@
 require 'fluent/input'
-require 'aws-sdk'
+require 'aws-sdk-rds'
+require 'aws-sdk-ec2'
 
 class Fluent::Plugin::RdsPgsqlLogInput < Fluent::Plugin::Input
   Fluent::Plugin.register_input('rds_pgsql_log', self)
